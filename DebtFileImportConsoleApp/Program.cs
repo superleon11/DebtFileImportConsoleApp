5b4a8be0501850437ff3e396a71fb8ad6e0028e4
Jordan O'Donnell <jordanodonnell93@gmail.com>
2026-02-05 10:24:22 +0000

    Initial code to handle -input command line argument

--- DebtFileImportConsoleApp/Program.cs
+++ DebtFileImportConsoleApp/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Validate command-line arguments, checks if the correct number of arguments is provided and if the first argument is "-input". If not, it displays an error message and usage instructions.
+            if (args.Length != 2 || args[0].ToLower() != "-input")
+            {
+                Console.WriteLine("Error: Invalid arguments.");
+                Console.WriteLine("Usage: DebtFileImportConsoleApp.exe -input \"filename.csv\"");
+                Console.WriteLine("OR");
+                Console.WriteLine("Usage: DebtFileImportConsoleApp.exe -input \"filename.txt\"");
+                return; 
+            }
+
+            string filePath = args[1];
+
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"Error: File '{filePath}' not found.");
+                return;
+            }
+
+            string extension = Path.GetExtension(filePath).Trim();
+            string[] allowedExtensions = { ".csv", ".txt" };
+
+            if (!allowedExtensions.Contains(extension, StringComparer.OrdinalIgnoreCase))
+            {
+                Console.WriteLine($"Error: File '{extension}' is not a valid .csv or .txt file.");
+                Console.WriteLine("Error: The file must either be a .csv or .txt format.");
+                return;
+            }
+
+            ProcessInputFile(filePath);
         }
+
+
+
+
+        static void ProcessInputFile(string filePath)
+        {
+            
+
+
+        }
+
     }
 }
