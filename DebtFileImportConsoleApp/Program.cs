a88a77d6260c10cfcef562ee551184eafec0210b
Jordan O'Donnell <jordanodonnell93@gmail.com>
2026-02-05 10:47:28 +0000

    Simple CSV/TXT file parsing logic to see input was being accepted correctly

--- DebtFileImportConsoleApp/Program.cs
+++ DebtFileImportConsoleApp/Program.cs
@@ -11,21 +11,26 @@
                 Console.WriteLine("Usage: DebtFileImportConsoleApp.exe -input \"filename.csv\"");
                 Console.WriteLine("OR");
                 Console.WriteLine("Usage: DebtFileImportConsoleApp.exe -input \"filename.txt\"");
-                return; 
+                return;
             }
+
 
             string filePath = args[1];
 
 
+            //Checks if the file passed in as an arguement exists, if it doesnt then it displays an error
             if (!File.Exists(filePath))
             {
                 Console.WriteLine($"Error: File '{filePath}' not found.");
                 return;
             }
 
+            //Gets the file extension of the file to be parsed.
             string extension = Path.GetExtension(filePath).Trim();
+            //Sets the allowed file extensions to be parsed
             string[] allowedExtensions = { ".csv", ".txt" };
 
+            //Checks if the file extension is either .csv or .txt, if not then it displays an error message.
             if (!allowedExtensions.Contains(extension, StringComparer.OrdinalIgnoreCase))
             {
                 Console.WriteLine($"Error: File '{extension}' is not a valid .csv or .txt file.");
@@ -33,18 +38,55 @@
                 return;
             }
 
-            ProcessInputFile(filePath);
+
+            ProcessInputFile(filePath, extension);
         }
 
 
 
 
-        static void ProcessInputFile(string filePath)
+        static void ProcessInputFile(string filePath, string extension)
         {
-            
+            //Uses this to set what the delimiter will be based on the file extension.
+            var delimiter = "";
+            if (extension.Equals(".csv", StringComparison.OrdinalIgnoreCase))
+            {
+                delimiter = ",";
+            }
+            else if (extension.Equals(".txt", StringComparison.OrdinalIgnoreCase))
+            {
+                delimiter = "|";
+            }
 
 
+            var lines = File.ReadLines(filePath).ToList();
+            if (lines.Count == 0)
+            {
+                Console.WriteLine("Error: File is empty. Please upload file with data.");
+                return;
+            }
+
+            Console.WriteLine("Processing records...");
+            foreach (var line in lines.Skip(1))
+            {
+                if (string.IsNullOrWhiteSpace(line)) continue;
+
+                var columns = line.Split(delimiter);
+
+
+                string accountNumber = columns[0].Trim();
+                string name = columns[1].Trim();
+                string amount = columns[2].Trim();
+                string phone = columns[3].Trim();
+
+                // Perform your business logic here
+                Console.WriteLine($"Found: {name} ({accountNumber}) - Amount: {amount} - Phone number: {phone}");
+            }
         }
+
+
+
 
     }
 }
+
